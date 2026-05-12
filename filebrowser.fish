c389138390dd10f77e77e7eef2fd6a26736c800a
oem <fatihyegen.09@gmail.com>
2026-05-12 15:56:25 +0300

    add: filebrowser global added. Some auto installations ignored.

---
+++ filebrowser.fish
@@ -0,0 +1,4 @@
+function filebrowser --description 'alias filebrowser=filebrowser -d /home/oem/.fbrowser/filebrowser.db'
+ command filebrowser -d /home/oem/.fbrowser/filebrowser.db $argv
+        
+end
